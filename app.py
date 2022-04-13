5599223b39e2ddbcaff4d664f6ecec9b6d29744c
Dalis Chan <dalischan97@gmail.com>
2022-04-13 20:17:26 +0800

    First commit

---
+++ app.py
@@ -0,0 +1,39 @@
+#app.py
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from pyspark.sql import SparkSession
+from pyspark.ml.classification import RandomForestClassificationModel
+from pyspark.ml.feature import VectorAssembler
+
+app = Flask(__name__)
+
+spark = SparkSession.builder.appName("FlightDelayPrediction").getOrCreate()
+sc = spark.sparkContext
+    # .config("spark.some.config.option", "some-value") \
+    
+model = RandomForestClassificationModel.load("/model")
+
+@app.route('/')
+def home():
+    return "<p>Hello, World!</p>"
+
+@app.route('/predict/<value_list>', methods=['GET'])
+def predict(value_list):
+    '''
+    For rendering results on HTML GUI
+    '''
+    arr_value = []
+    values = value_list.split(',')
+    for value in values:
+        arr_value.append(float(value))
+    inputCols =  ['WeatherScore', 'Reporting_Airline_Index', 'Origin_Index', 'Dest_Index', 'MFR_Index', "sum_passengers_airport_month"]
+    assembler = VectorAssembler(inputCols=inputCols, outputCol="features")
+    df = spark.createDataFrame([tuple(arr_value)], inputCols)
+    featuresMLData = assembler.transform(df)
+    prediction_result = model.predict(featuresMLData.head().features)
+    return jsonify(
+        value_input=value_list,
+        result=prediction_result
+    )
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0')
